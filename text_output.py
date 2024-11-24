b6a798da321d970ad56be5c893ed1b9e2b4336eb
BongAchiver <tibonmulion@gmail.com>
2024-11-24 15:20:25 +1000

    First Add

---
+++ text_output.py
@@ -0,0 +1,19 @@
+import cv2
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = 'Tesseract\\tesseract.exe'
+
+# Подключение фото
+img = cv2.imread('ВАШ ФАЙЛ СОЗДАННЫЙ scanner.py')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Будет выведен весь текст с картинки
+config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(img, lang='eng+rus', config=config)
+
+# Запись результата в файл result.txt
+with open('result.txt', 'w', encoding='utf-8') as file:
+    file.write(text)
+
+print("Результат сохранен в файл result.txt")
